8cf33c2ccb024e1f46548b840b4331467d10c97e
Davide Fucci <davfuc@gmail.com>
2018-06-21 17:28:49 +0200

    fix endpoints
    based on http://geoffreylessel.com/2016/hosting-a-phoenix-app-in-a-subdirectory-with-nginx/

--- lib/ex_vote_web/endpoint.ex
+++ lib/ex_vote_web/endpoint.ex
@@ -8,7 +8,7 @@ defmodule ExVoteWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :ex_vote, gzip: false,
+    at: "/hitec/edemocracy", from: :ex_vote, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
