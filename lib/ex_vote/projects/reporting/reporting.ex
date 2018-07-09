f8ed4b68f75a0ca36885c9263716da4f720e6a0d
Frederik Born <born@born-solutions.de>
2018-07-09 20:09:46 +0200

    Extended report generation

--- lib/ex_vote/projects/reporting/reporting.ex
+++ lib/ex_vote/projects/reporting/reporting.ex
@@ -12,10 +12,12 @@ defmodule ExVote.Projects.Reporting do
       |> Map.update(:candidates, [], &Repo.preload(&1, votes_candidate: [:ticket]))
       |> Map.update(:users, [], &Repo.preload(&1, :vote_user))
 
+    project = Repo.preload(project, :tickets)
+
     %Report{}
     |> populate_schedule(project)
     |> populate_participations(participations)
-    |> populate_votes(participations)
+    |> populate_votes(project, participations)
   end
 
   defp participation_type(%UserParticipation{}), do: :users
@@ -55,28 +57,42 @@ defmodule ExVote.Projects.Reporting do
   defp voted?(%CandidateParticipation{votes_candidate: votes}) when length(votes) === 0, do: false
   defp voted?(%CandidateParticipation{}), do: true
 
-  defp populate_votes(report, %{users: users, candidates: candidates}) do
-    tickets =
+  defp populate_votes(report, %{tickets: tickets}, %{users: users, candidates: candidates}) do
+    ticket_votes =
       candidates
       |> format_votes()
-      |> Enum.map(fn [head | _] = votes_for_ticket ->
-        %{
-          ticket: head,
-          votes_received: length(votes_for_ticket)
-        }
-      end)
+      |> Enum.map(fn [head | _] = votes -> vote_container(:ticket, head, votes) end)
 
-    candidates =
+    missing_ticket_votes =
+      tickets
+      |> Enum.reject(fn %{id: id} ->
+        Enum.any?(ticket_votes, fn %{ticket: %{id: vote_id}} -> id === vote_id end)
+      end)
+      |> Enum.map(&vote_container(:ticket, &1, 0))
+
+    ticket_votes =
+      ticket_votes
+      |> Enum.concat(missing_ticket_votes)
+      |> Enum.sort(&compare_votes/2)
+
+    candidate_votes =
       users
       |> format_votes()
-      |> Enum.map(fn [head | _] = votes_for_candidate ->
-        %{
-          candidate: head,
-          votes_received: length(votes_for_candidate)
-        }
-      end)
+      |> Enum.map(fn [head | _] = votes -> vote_container(:candidate, head, votes) end)
 
-    %Report{report | votes: %{tickets: tickets, candidates: candidates}}
+    missing_candidate_votes =
+      candidates
+      |> Enum.reject(fn %{user: %{id: id}} ->
+        Enum.any?(candidate_votes, fn %{candidate: %{id: vote_id}} -> id === vote_id end)
+      end)
+      |> Enum.map(fn %{user: candidate} -> vote_container(:candidate, candidate, 0) end)
+
+    candidate_votes =
+      candidate_votes
+      |> Enum.concat(missing_candidate_votes)
+      |> Enum.sort(&compare_votes/2)
+
+    %Report{report | votes: %{tickets: ticket_votes, candidates: candidate_votes}}
   end
 
   defp format_votes(participations) do
@@ -91,4 +107,17 @@ defmodule ExVote.Projects.Reporting do
 
   defp accumulate_votes(%CandidateParticipation{votes_candidate: votes}, acc),
     do: Enum.map(votes, &Map.get(&1, :ticket)) ++ acc
+
+  defp vote_container(type, value, votes) when is_list(votes),
+    do: vote_container(type, value, length(votes))
+
+  defp vote_container(type, value, votes) do
+    %{
+      type => value,
+      :votes_received => votes
+    }
+  end
+
+  defp compare_votes(%{votes_received: votes_left}, %{votes_received: votes_right}),
+    do: votes_left >= votes_right
 end
