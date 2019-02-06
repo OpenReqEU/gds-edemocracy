a9fe9f04ccc7bf5d86638ca969d29a331bfb6414
Davide Fucci <davfuc@gmail.com>
2019-02-06 11:47:09 +0100

    updates elixir docker image

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-FROM elixir:alpine
+FROM elixir:1.8.1-alpine
 WORKDIR /ex_vote
 COPY . /ex_vote
 
 ENV MIX_ENV=docker
-
 RUN mix local.hex --force
+# RUN mix deps.get
 RUN HEX_HTTP_CONCURRENCY=1 HEX_HTTP_TIMEOUT=120 mix deps.get
 RUN mix local.rebar --force
-#RUN mix deps.get
 RUN mix deps.compile
 
 EXPOSE 9750
--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule ExVote.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
+      {:gettext, "~> 0.16.0"},
       {:cowboy, "~> 1.0"},
       {:timex, "~> 3.1"},
       {:phoenix_swagger, "~> 0.8"},
